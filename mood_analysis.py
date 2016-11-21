30f9c6a08ab45da342c06c7c0982b02e7718cb7e
Paulina Ng <qwnpng@gmail.com>
2016-11-21 23:22:15 +0000

    Added a file for mood data anlysis functions

---
+++ mood_analysis.py
@@ -0,0 +1,19 @@
+from model import User
+# import numpy as np
+import pandas as pd
+
+
+def get_rolling_mean(user_id, r_window=60):
+    """ Gets rolling mean of mood rating for a user"""
+    user = User.query.get(user_id)
+    days = user.days
+    overall_moods = [day.overall_mood for day in days]
+
+    moods = pd.Series(overall_moods)
+    r = moods.rolling(window=r_window)
+    r_mean_moods = r.mean()
+    r_mean_mood_list = [int(mean) for mean in r_mean_moods[(r_window-1):]]
+    mood_list = list(moods[:(r_window-1)]) + r_mean_mood_list
+    smooth = pd.Series(mood_list, index=[day.date for day in days])
+
+    return smooth
