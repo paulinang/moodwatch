33a614e4e3410c3c54b27ca01b0d3b6ea5daa304
Paulina Ng <qwnpng@gmail.com>
2016-11-25 05:32:30 +0000

    Cleaned up pro_dashboard, moved js fx over to main.js

--- static/main.js
+++ static/main.js
@@ -55,3 +55,71 @@ function displaySearchResults(requestedDay) {
         }
     });
 }
+
+
+/// GET CLIENT DATA
+ function showClientData(clientId, proUsername) {
+    $.get('/client_prescriptions.json', {client_id: clientId}, function(data) {
+        $('.client-details').show()
+        $('#client-name').html(data.username)
+        var prescriptions = data.prescriptions
+        var medText = ''
+        for (var key in prescriptions) {
+            // For each drug type in prescriptions add an unordered list element
+            // Start an ordered list under that element
+            medText = medText + '<li>' + key + '</li><ol>';
+            var prescriptionArray = prescriptions[key];
+            for (i = 0; i < prescriptionArray.length; i++) {
+                // For each prescription of a drug, add it as an ordered list element
+                var prescription = prescriptionArray[i];
+                medText = medText + '<li>Started ' + prescription.start_date;
+                // Label if it's active
+                if (!prescription.end_date) {
+                    medText = medText + ' [ACTIVE]';
+                    if (prescription.pro_username == proUsername) {
+                        medText = medText + '&nbsp;&nbsp;&nbsp;<button type="button" class="btn btn-primary btn-lg change-prescription-button" data-toggle="modal" data-target="#change-prescription-modal" data-prescription-id="' + prescription.prescription_id + '" data-drug-id="' + prescription.drug_id + '"">Change Prescription</button>';
+                    }
+                }
+                medText = medText + '</li><ul>';
+
+                if (prescription.end_date) {
+                    medText = medText + '<li>Ended: ' + prescription.end_date;
+                }
+                medText = medText + '<li>Prescribed by: ' + prescription.pro_username;
+                medText = medText + '</li><li>Instructions: ' + prescription.instructions;
+                if (prescription.notes) {
+                    medText = medText + '</li><li>Notes: ' + prescription.notes;
+                }
+                medText = medText + '</li></ul>'
+            }
+        medText = medText + '</li></ol>'
+        
+        }
+        medText = medText + '</ul>';
+        $('#client-meds').html(medText);
+    });
+    if (moodChart) {
+        moodChart.destroy();
+    }
+
+    var currentYear = [
+        moment().startOf('year').format('YYYY-MM-DD'),
+        moment().endOf('year').format('YYYY-MM-DD')
+        ];
+    createClientChart(currentYear[0], currentYear[1], clientId);
+}
+
+
+// END PRESCRIPTION
+function endPrescription(prescriptionId) {
+    $.post('/end_prescription.json',
+        {prescriptionId: prescriptionId, currentDate: $('#start-date').val()},
+        function(data) {});
+}
+
+//ADD PRESCRIPTION
+function addPrescription() {
+    $.post('/add_prescription.json', formInputs, function(data) {
+        $('#change-prescription-modal').modal('hide');
+    });
+}
